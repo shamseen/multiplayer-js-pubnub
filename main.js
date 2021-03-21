d784403f074754439b2459c90114440c4ad20673
shamseen <shamseen.c@gmail.com>
2021-03-20 21:42:03 -0400

    starting game lobby

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 console.log("JS linked!");
+
+/* We're gonna pretend this is lobby.js */
+// tutorial keeps all logic in anon function ¯\_(ツ)_/¯
+
+// upon player entering the lobby:
+(function () {
+    // TO DO: lobby code
+
+})(); // the end (); calls the anon function :o
